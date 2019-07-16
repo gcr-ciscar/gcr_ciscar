1bea80f827eff5eeff786484a3af587dccec43f2
pgermain <pgermain@P-4H26RQ2-34.GCRPLUS.local>
2019-07-16 16:39:02 +0200

    Premier Commit

---
+++ config/configuration.php
@@ -0,0 +1,24 @@
+<?php
+
+// MySQL Variable
+
+// $CONFIG_MYSQL_HOSTNAME = '217.119.135.21';
+$CONFIG_MYSQL_HOSTNAME = '127.0.0.1';
+
+//VM DEV
+//$CONFIG_MYSQL_USERNAME = 'abakus';
+//$CONFIG_MYSQL_PASSWORD = 'HjMPcLr9';
+
+//LOCAL
+$CONFIG_MYSQL_USERNAME = 'root';
+$CONFIG_MYSQL_PASSWORD = '';
+
+// $CONFIG_MYSQL_BASENAME = 'base-developpement-v3';
+$CONFIG_MYSQL_BASENAME = 'base-production-v3';
+
+$CONFIG_MYSQL_BASE_ATT_ACNF = 'attachment_ACNF';
+$CONFIG_MYSQL_BASE_ATT_CISCAR = 'attachment_CISCAR';
+$CONFIG_MYSQL_BASE_ATT_GCR = 'attachment_GCR';
+$CONFIG_MYSQL_BASE_ATT_GCRE = 'attachment_GCRE';
+
+?>
